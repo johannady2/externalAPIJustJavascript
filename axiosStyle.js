ec9d7f7d007941577453a4f02c91c9c7593165dc
johannady2 <johannady2@gmail.com>
2026-03-20 09:53:58 +0900

    added notes comments

--- axiosStyle.js
+++ axiosStyle.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-qpp.get("/", async (req,res=>
+qpp.get("/", async (req,res)=>
 {
     try
     {
         const response = await axios.get("https://bored-api.appbrewery.com/random");
-        res.render("index.ejs", {activity: response.data});
+        res.render("index.ejs", {activity: response.data});//we don't need to use json.parse if we use axios
     }
     catch(error)
     {
